48c677c6b566ec3b953ea5f3d68b93e71a2b1100
Fredrik Landström <fredrik.landstrom@compose.se>
2014-02-10 11:07:13 +0100

    added example code

--- js/script.js
+++ js/script.js
@@ -1,3 +1,28 @@
 $( document ).ready(function() {
-	// Begin
+	var data=[
+			{
+			"divid":"AD",
+			"data":{"Availability": "7","Capacity": "2","Event": "7","Performance": "15"}
+			},
+			{
+			"divid":"Biztalk",
+			"data":{"Availability": "12","Event": "4","Performance": "2"}
+			}
+		];
+	// empty the contaner
+	$("#graphcontainer").empty();
+
+	// Loop through all the intems in the array
+	$.each(data,function(index, item){
+		var divId=item.divid;	// id of the new div
+		var grapdata=item.data; // data for the graph
+		var containerToPopulate=$("#graphcontainer"); // find the parent div to exetend
+		var divhtml="<div id=\""+divId+"\"></div>" // create the html for the div
+		containerToPopulate.append(divhtml); // append the new div to the parent container
+
+		// the new div now exist and can be modified with grapdata
+		var graphIdcontainer=$("#"+divId);
+		// proof
+		graphIdcontainer.html("Im now in the "+divId+" container");
+	});
 });
